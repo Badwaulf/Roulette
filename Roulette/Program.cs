f0f8eff2963d07e8c95478f7e7f6cd5559ee66d2
Administrateur <Administrateur@UTILISA-EEOHTA2>
2018-11-12 16:55:05 +0100

    Ajoutez des fichiers projet.

---
+++ Roulette/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestInteger joueur = new TestInteger();
+
+            int stackBanque = 666;
+
+            Console.WriteLine("Do you want a play a game??");
+            Console.ReadLine();
+     
+            Console.WriteLine("<-********************************************->");
+            Console.WriteLine("<-*******Ceci est le jeu de la roulette*******->");
+            Console.WriteLine("<-********************************************->");
+
+            //Choix du joueur de son stack
+            var stack = joueur.GetStack(stackBanque);
+
+            while (stackBanque != 0 && stack != 0)
+            {
+                Random valeur = new Random();
+                int aleatoire = valeur.Next(0, 36);
+                
+
+                //Choix du joueur du chiffre sur lequel il mise
+                var number = joueur.GetNumber();
+
+                //Choix du joueur de la mise
+                var mise = joueur.GetMise(stack, number);
+
+                if (number == aleatoire)
+                {
+                    Console.WriteLine("(°v°) Mazeltof!! Vous avez gagné ... Votre stack augmente maintenant de " + mise * 36);
+                    Console.WriteLine("Votre stack est actuellement de ===> " + (stack + (mise * 36)));
+                }
+                else if (aleatoire == number && number == 0)
+                {
+                    Console.WriteLine("(°v°) Mazeltof!! Vous avez gagné ... Votre stack augmente maintenant de " + mise * 50);
+                    Console.WriteLine("Votre stack est actuellement de ===> " + (stack + (mise * 50)));
+                }
+                else if (aleatoire % 2 == 0 &&  number %2 == 0)
+                {
+                    Console.WriteLine("(°v°) Mazeltof!! Vous avez gagné ... Votre stack augmente maintenant de " + mise * 20);
+                    Console.WriteLine("Votre stack est actuellement de ===> " + (stack + (mise * 20)));
+                }
+                else if (aleatoire % 2 != 0 && number %2 != 0)
+                {
+                    Console.WriteLine("(°v°) Mazeltof!! Vous avez gagné ... Votre stack augmente maintenant de " + mise * 20);
+                    Console.WriteLine("Votre stack est actuellement de ===> " + (stack + (mise * 20)));
+                }
+                else
+                    Console.WriteLine("La roulette avait choisis le : " + aleatoire + " Il vous reste : " + (stack = stack - mise) + " dans votre stack " +
+                        "\n Même joueur joue encore!");
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Roulette/TestInteger.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    class TestInteger
+    {
+        
+        public int GetStack(int stackBanque)
+        {
+            int NombreStack = 0;
+            bool choix = false;
+
+            while (choix == false)
+            {
+                Console.WriteLine("\n =^.^= Choisir votre stack entre 0 et 666 =^.^=");
+                string NbreSaisiStack = Console.ReadLine();
+
+                if (int.TryParse(NbreSaisiStack, out NombreStack))
+                {
+                    choix = true;
+                    if (NombreStack >= 0 && NombreStack <= stackBanque)
+                    {
+                        Console.WriteLine("Vous avez choisis d'avoir " + NombreStack + " dans votre stack");
+                        return NombreStack;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Merci de faire une demande de stack entre 0 et 666 =^.^=");
+                        choix = false;
+                    }
+
+                }
+                else
+                {
+                    choix = false;
+                    Console.WriteLine("Erreur! le nombre saisie n'est pas un chiffre entier!:!:!:!");
+
+
+                }
+
+
+
+            }
+            return NombreStack;
+
+        }
+        
+        /********************************************************************************************/
+
+
+        public int GetNumber()
+        {
+            int NombreChoisi = 0;
+            bool choixnombre = false;
+
+            while (choixnombre == false)
+            {
+                Console.WriteLine("\n =^.^= Choisir le chiffre sur lequel miser entre 0 et 36  =^.^=");
+                string NbreSaisiChoisi = Console.ReadLine();
+
+                if (int.TryParse(NbreSaisiChoisi, out NombreChoisi))
+                {
+                    choixnombre = true;
+                    if (NombreChoisi >= 0 && NombreChoisi <= 36)
+                    {
+                        Console.WriteLine("Vous avez choisis de miser sur " + NombreChoisi );
+                        return NombreChoisi;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Merci de faire une mise entre 0 et 36 =^.^=");
+                        choixnombre = false;
+                    }
+
+                }
+                else
+                {
+                    choixnombre = false;
+                    Console.WriteLine("Erreur! le nombre saisie n'est pas un chiffre entier!:!:!:!");
+
+
+                }
+
+
+
+            }
+            return NombreChoisi;
+        }
+
+        /********************************************************************************************/
+
+
+        public int GetMise(int stack, int number)
+        {
+            int NombreMise = 0;
+            bool choixmise = false;
+
+            while (choixmise == false)
+            {
+                Console.WriteLine("\n =^.^= Choisir le montant de votre mise =^.^=");
+                string NbreSaisiMise = Console.ReadLine();
+
+                if (int.TryParse(NbreSaisiMise, out NombreMise))
+                {
+                    choixmise = true;
+                    if (NombreMise <= stack)
+                    {
+                        Console.WriteLine("\n Vous avez choisis de miser " + NombreMise +" sur " + number);
+                        
+                        return NombreMise;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Merci de faire une mise entre 0 et " + stack +" =^.^=");
+                        choixmise = false;
+                    }
+
+                }
+                else
+                {
+                    choixmise = false;
+                    Console.WriteLine("Erreur! le nombre de la mise n'est pas un chiffre entier!:!:!:!");
+
+
+                }
+
+
+
+            }
+            return NombreMise;
+        }
+
+
+    }
+}
